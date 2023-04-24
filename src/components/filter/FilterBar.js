e30424b19b9e8b1c2038421cc34cead87b9f9112
Ekansh Gupta <103844989+ekanshgupta981@users.noreply.github.com>
2023-04-24 11:57:56 +0530

    Update FilterBar.js

--- src/components/filter/FilterBar.js
+++ src/components/filter/FilterBar.js
@@ -91,96 +91,7 @@ export default function FilterBar({ filterProducts, filter }) {
             </div>
             {/* ===== filter ===== */}
             <div className="col-lg-6 col-12 mt-lg-0 mt-2 d-flex align-items-center flex-wrap">
-//               <button
-//                 className="btn btn-outline-light px-3 mt-2 mx-2 fs-5 "
-//                 data-bs-toggle="modal"
-//                 data-bs-target="#exampleModal"
-//               >
-//                 <i className="fa fa-tasks" aria-hidden="true"></i> Filters
-//               </button>
-//               <form action="">
-//                 <div
-//                   className="modal fade"
-//                   id="exampleModal"
-//                   tabIndex="-1"
-//                   aria-labelledby="exampleModalLabel"
-//                   aria-hidden="true"
-//                 >
-//                   <div className="modal-dialog modal-lg mt-5">
-//                     <div className="modal-content">
-//                       <div className="modal-header">
-//                         <h5
-//                           className="modal-title text-dark fs-3"
-//                           id="exampleModalLabel"
-//                         >
-//                           Filters
-//                         </h5>
-//                         <button
-//                           type="button"
-//                           className="btn-close"
-//                           data-bs-dismiss="modal"
-//                           aria-label="Close"
-//                         ></button>
-//                       </div>
 
-                      
-
-//                       <section className="d-flex shop-filter-modal">
-//                         <div className="col-4 border">
-//                           <div className="border-bottom">
-//                             <h5
-//                               className={
-//                                 filterToggle === true
-//                                   ? "text-dark p-2 py-5 border-start border-4 border-danger mb-0"
-//                                   : "text-dark p-2 py-5 border mt-0 mb-0"
-//                               }
-//                               onClick={() => setFilterToggle(true)}
-//                             >
-//                               Category
-//                             </h5>
-//                           </div>
-//                           <div className="border-bottom">
-//                             <h5
-//                               className={
-//                                 filterToggle === false
-//                                   ? "text-dark p-2 py-5 border-start border-4 border-danger mb-0"
-//                                   : "text-dark p-2 py-5 border mt-0 mb-0"
-//                               }
-//                               onClick={() => setFilterToggle(false)}
-//                             >
-//                               Price Range
-//                             </h5>
-//                           </div>
-//                         </div>
-
-//                         {/* ===== filter content ===== */}
-
-//                         <div className="col-8 text-dark">
-//                           {filterToggle === true ? (
-//                             <FilterCat filterData={filterData} />
-//                           ) : (
-//                             <FilterPriceRange filterData={filterData} />
-//                           )}
-//                         </div>
-//                       </section>
-//                       <div className="modal-footer">
-//                         <input
-//                           type="reset"
-//                           value="Clear"
-//                           className="bg-transparent text-muted border-0 btn"
-//                         />
-//                         <button
-//                           type="button"
-//                           className="btn btn-danger px-5"
-//                           onClick={() => Apply(apply)}
-//                         >
-//                           Apply
-//                         </button>
-//                       </div>
-//                     </div>
-//                   </div>
-//                 </div>
-//               </form>
               
               <div>
                 {veg === "" ? (
@@ -243,3 +154,4 @@ export default function FilterBar({ filterProducts, filter }) {
     </>
   );
 }
+
